17558c3380a41d6ca3628b3eb01879d75dea1674
zalaxis <imnotchxvi@gmail.com>
2026-08-27 20:21:45 +0200

    sveltekit template

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import favicon from '$lib/assets/favicon.png';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
