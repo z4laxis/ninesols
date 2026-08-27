24a55392ab98227c9aaf5dc5b46f9fba5ecee277
zalaxis <imnotchxvi@gmail.com>
2026-08-27 20:48:08 +0200

    drool

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
+<script>
+  import { MetaTags } from 'svelte-meta-tags';
+</script>
+
+<MetaTags
+  title="Nine Sols"
+  description="A short description of this page."
+  canonical="https://example.com/my-page"
+  openGraph={{
+    title: 'Nine Sols',
+    description: 'A short description of this page.',
+    images: [{ url: 'https://example.com/og-image.jpg', width: 1200, height: 630, alt: 'Nine Sols' }]
+  }}
+/>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
