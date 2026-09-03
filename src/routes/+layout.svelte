4cf93f3f46e536e4d70d2d2fd03d74a8c996e2f6
zalaxis <imnotchxvi@gmail.com>
2026-09-03 22:25:37 +0200

    added main menu bg

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,20 @@
 <script>
+	import { MetaTags } from 'svelte-meta-tags';
 	import favicon from '$lib/assets/favicon.png';
 
 	let { children } = $props();
 </script>
+
+<MetaTags
+  title="Nine Sols"
+  description="A short description of this page."
+  canonical="https://example.com/my-page"
+  openGraph={{
+    title: 'Nine Sols',
+    description: 'A short description of this page.',
+    images: [{ url: 'https://example.com/og-image.jpg', width: 1200, height: 630, alt: 'Nine Sols' }]
+  }}
+/>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
