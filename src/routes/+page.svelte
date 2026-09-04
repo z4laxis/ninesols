7f78083fed66b0abcd954931ea66fe117aaa02ce
zalaxis <imnotchxvi@gmail.com>
2026-09-05 01:21:04 +0200

    started working on main menu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,25 @@
 <script>
   import { MetaTags } from 'svelte-meta-tags';
+
   import logo from '$lib/assets/img/logo.png';
-  import sandbox from '$lib/assets/img/bg/True_Ending_BG.jpg';
+  import bg from '$lib/assets/img/bg/New_Game_BG.jpg';
+  import sandbox from '$lib/assets/img/bg/Normal_BG.jpg';
 </script>
+
+<style>
+  :global(body) {
+    margin: 0;
+    overflow: hidden;
+  }
+
+  .logo {
+    width: 13%;
+    position: absolute;
+    z-index: 1;
+    left: 13%;
+    top: 11%;
+  }
+</style>
 
 <MetaTags
   title="Nine Sols"
@@ -15,4 +32,5 @@
   }}
 />
 
-<img alt="The project logo" src={logo} style="width: 100%; height: auto;" />S
+<img alt="Nine Sols Logo" src={logo} class="logo" />
+<img alt="Sandbox Background" src="https://www.gameuidatabase.com/uploads/Nine-Sols12282024-070345-33433.jpg" style="width: 100%; height: auto;" />
